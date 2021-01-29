2a4edd71903dde91e59118b90b5cff126501d6bc
child_duckling <19170969+child-duckling@users.noreply.github.com>
2021-01-28 18:57:35 -0800

    got icons + stuff

--- main.js
+++ main.js
@@ -6,25 +6,38 @@
 // https://electronjs.org/docs/api/browser-view
 
 // In the main process.
-const { BrowserView, BrowserWindow, app, MenuItem, Menu, dialog } = require('electron')
+const { BrowserView, BrowserWindow, app, dialog } = require('electron')
+const Menu = require("electron-create-menu")
 const Store = require('electron-store');
 const prompt = require('electron-prompt');
+const settings = require('electron-settings')
+const storage = require('electron-json-storage')
+
+storage.setDataPath(app.getPath('appData'))
+var fullyLoaded = false
 
 var currentCameraURL
 var currentCameraTitle
 const favorites = new Store();
 var favoritesName = []
 var indexPage = 'https://cwwp2.dot.ca.gov/vm/streamlist.htm'
-
-//RGB for text - Will default to black when transparentCameraWindow is false
+    //RGB for text - Will default to black when transparentCameraWindow is false
 var textColor = '255,255,255'
-var transparentCameraWindow = false
+var transparentCameraWindow = true
+var resetStoreAfterOpen = true
+
+
+
+
 
 if (transparentCameraWindow === false) {
     textColor = '0,0,0'
+} else if (resetStoreAfterOpen === true) {
+    favorites.clear()
 }
 
 app.whenReady().then(() => {
+    setupMenu()
     let win = new BrowserWindow({ width: 1100, height: 500, webPreferences: { webSecurity: false } })
 
     win.on('closed', () => {
@@ -40,21 +53,24 @@ app.whenReady().then(() => {
 
     win.setBrowserView(view)
     win.loadURL(indexPage)
-
+    fullyLoaded = true
     win.on('page-title-updated', () => {
         globalThis.win2 = new BrowserWindow({ width: 310, height: 425, transparent: transparentCameraWindow, webPreferences: { webSecurity: false } })
         if (win.webContents.getURL() != indexPage || win.webContents.getURL() != 'about:blank') {
             win2.loadURL(win.webContents.getURL())
             win2.webContents.insertCSS('#wx{position:absolute;top:270px;width:320px;color:rgb(' + textColor + ');}')
+            currentCameraTitle = win2.getTitle()
             win.loadURL(indexPage)
         }
 
         win2.on('page-title-updated', () => {
+            win2.webContents.insertCSS('#wx{position:absolute;top:270px;width:320px;color:rgb(' + textColor + ');}')
             if (win2.webContents.getURL() == indexPage) {
                 win2.close()
-                updateFavorites()
             } else {
+                updateFavorites()
                 currentCameraURL = win2.webContents.getURL()
+                console.log("\n Camera name: " + currentCameraTitle + "\n Camera URL: " + currentCameraURL)
                 win2.webContents.insertCSS('#wx{position:absolute;top:270px;width:320px;color:rgb(' + textColor + ');}')
             }
         })
@@ -62,142 +78,41 @@ app.whenReady().then(() => {
 })
 
 function setFavorites() {
-    var setFav = prompt({ title: 'What Do You Want To Name It?', label: 'Name:', value: 'A nice nickname', inputAttrs: { type: 'text' }, type: 'input' })
-    favorites.set(setFav, currentCameraURL).catch(dialog.showErrorBox("You haven't clicked on a camera yet or something's wrong"))
-    favoritesName[favorites.size] = setFav
-    prompt({ title: "Saved!" })
+
+    //var fav = Object.create()
+    //storage.set(String(favorites.size), { 'name': prompt({ title: 'What Do You Want To Name It?', label: 'Name:', value: currentCameraTitle, inputAttrs: { type: 'text' }, type: 'input', alwaysOnTop: true }), 'url': currentCameraURL })
+    //.catch(dialog.showErrorBox("You haven't clicked on a camera yet or something's wrong"))
+    //console.log("Saved new favorite \n Current List of Favorites:")
+    //storage.keys(function(error, keys) {
+    //   if (error) throw error;
+
+    //   for (var key of keys) {
+    //        console.log(key);
+    //   }
+    //});
+
+
+
 }
 
 function updateFavorites() {
-    console.log(favorites.store)
-    var addToMenu = {}
-    try {
-        for (var i; i in favorites.size; i++) {
-            var menuName = favoritesName[i]
-            var link = favorites.get(menuName)
-            addToMenu.splice(1, 0, (MenuItem({ click: async() => { win.loadURL(link) }, name: menuName, label: menuName })))
-        }
-    } catch (error) {
-        (console.log("No Favorites to list"))
-    }
+    console.log(storage.getAll())
+}
 
-
-    template.splice(6, 1, {
-                label: 'Favorites',
-                role: 'Favorites',
-                submenu: [{
-                    label: 'Add to Favorites',
-                    click: async() => { setFavorites() }
-                }, addToMenu)
-            }
-
-
-
-
-
-
-
-
-
-
-
-            //Menu
-            const isMac = process.platform === 'darwin'
-
-            const template = [
-                // { role: 'appMenu' }
-                ...(isMac ? [{
-                    label: app.name,
-                    submenu: [
-                        { role: 'about' },
-                        { type: 'separator' },
-                        { role: 'services' },
-                        { type: 'separator' },
-                        { role: 'hide' },
-                        { role: 'hideothers' },
-                        { role: 'unhide' },
-                        { type: 'separator' },
-                        { role: 'quit' }
-                    ]
-                }] : []),
-                // { role: 'fileMenu' }
-                {
-                    label: 'File',
-                    submenu: [
-                        isMac ? { role: 'close' } : { role: 'quit' }
-                    ]
+function setupMenu() {
+    Menu()
+    Menu((defaultMenu, separator) => {
+        defaultMenu.push({
+            label: "Favorites",
+            submenu: [{
+                    label: "Add to Favorites",
+                    click: setFavorites()
                 },
-                // { role: 'editMenu' }
-                {
-                    label: 'Edit',
-                    submenu: [
-                        { role: 'undo' },
-                        { role: 'redo' },
-                        { type: 'separator' },
-                        { role: 'cut' },
-                        { role: 'copy' },
-                        { role: 'paste' },
-                        ...(isMac ? [
-                            { role: 'pasteAndMatchStyle' },
-                            { role: 'delete' },
-                            { role: 'selectAll' },
-                            { type: 'separator' },
-                            {
-                                label: 'Speech',
-                                submenu: [
-                                    { role: 'startspeaking' },
-                                    { role: 'stopspeaking' }
-                                ]
-                            }
-                        ] : [
-                            { role: 'delete' },
-                            { type: 'separator' },
-                            { role: 'selectAll' }
-                        ])
-                    ]
-                },
-                // { role: 'viewMenu' }
-                {
-                    label: 'View',
-                    submenu: [
-                        { role: 'reload' },
-                        { role: 'forcereload' },
-                        { role: 'toggledevtools' },
-                        { type: 'separator' },
-                        { role: 'resetzoom' },
-                        { role: 'zoomin' },
-                        { role: 'zoomout' },
-                        { type: 'separator' },
-                        //{ role: 'togglefullscreen' }
-                    ]
-                },
-                // { role: 'windowMenu' }
-                {
-                    label: 'Window',
-                    submenu: [
-                        { role: 'minimize' },
-                        { role: 'zoom' },
-                        ...(isMac ? [
-                            { type: 'separator' },
-                            { role: 'front' },
-                            { type: 'separator' },
-                            { role: 'window' }
-                        ] : [
-                            { role: 'close' }
-                        ])
-                    ]
-                },
-                {
-                    role: 'help',
-                    submenu: [{
-                        label: 'Learn More',
-                        click: async() => {
-                            const { shell } = require('electron')
-                            await shell.openExternal('https://electronjs.org')
-                        }
-                    }]
-                }
+                separator(),
+                { label: "my second item" }
             ]
+        })
 
-            const menu = Menu.buildFromTemplate(template)
-            Menu.setApplicationMenu(menu)
+        return defaultMenu
+    })
+}
