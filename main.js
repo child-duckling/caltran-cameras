226269435e40c652e8be800d99f0a9e5cddc70ea
child_duckling <19170969+child-duckling@users.noreply.github.com>
2021-02-18 12:31:20 -0800

    Actually patched the window bug. Forgot to change local var to global

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ app.whenReady().then(() => {
     win.setIcon('icon.png')
     fullyLoaded = true
     win.on('page-title-updated', () => {
-        GlobalThis.win2 = new BrowserWindow({ width: 310, height: 425, transparent: transparentCameraWindow, frame: winOnlyNotTransFrame, webPreferences: { webSecurity: false }, alwaysOnTop: true })
+        globalThis.win2 = new BrowserWindow({ width: 310, height: 425, transparent: transparentCameraWindow, frame: winOnlyNotTransFrame, webPreferences: { webSecurity: false }, alwaysOnTop: true })
         if (win.webContents.getURL != indexPage) {
 
             win2.loadURL(win.webContents.getURL())
