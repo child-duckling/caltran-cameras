071611908f3dc3623ae36a343e72717a129a0811
child_duckling <19170969+child-duckling@users.noreply.github.com>
2021-04-05 11:20:33 -0700

    revamp

---
+++ web/main.js
@@ -0,0 +1,52 @@
+document.addEventListener('mousedown', function(e) {
+    //var selectionActions = document.getElementsByClassName('selectionActions');
+    var isSelected = document.getElementsByClassName('is-selected');
+    if (isSelected.length != 0) {
+        toggleActions('show');
+    } else {
+        toggleActions('hide');
+    }
+    //console.log(document.links)
+
+
+});
+
+function selectToast() {
+    var snack = document.querySelector('.mdl-js-snackbar');
+    var data = {
+        name: 'Add to Favorites',
+        actionHandler: getSelected(),
+        actionText: 'Add to Favorites',
+        timeout: 10000
+    };
+    snack.MaterialSnackbar.showSnackbar(data);
+}
+
+function toggleActions(a) {
+    if (a == 'show') {
+        document.getElementById("selectionActions").style = "";
+        document.getElementById("table").style = "width:950px";
+    } else {
+        document.getElementById("selectionActions").style = "display: none";
+        document.getElementById("table").style = "width:875px";
+    }
+
+
+}
+
+function addToFavs() {
+    console.log('aaaaaaaaaaaaaaaaa')
+
+
+}
+
+function getSelected() {
+    var isSelected = document.getElementsByClassName('is-selected');
+    for (var i; i >= isSelected.length; i++) {
+        console.log(isSelected);
+
+    }
+
+
+
+}
